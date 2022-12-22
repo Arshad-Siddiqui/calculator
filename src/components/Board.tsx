efdad6cf22f65d705d0f0e0a2d4afc498aacff54
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-22 21:53:54 +0000

    Have Display use state

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,12 @@
 import Numpad from "./Numpad";
 import Display from "./Display";
+import { useState } from "react";
 
 export default function Board() {
+  const [display, setDisplay] = useState(0);
   return (
     <div className="board">
-      <Display children={0}/>
+      <Display children={display}/>
       <Numpad />
     </div>
   );
