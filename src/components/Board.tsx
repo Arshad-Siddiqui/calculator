d30eb7ad6f40dc84120dc0a50e90666b9917be1a
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-22 19:42:47 +0000

    Have Board represent a button

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,14 @@
+import Button from './Button'
+
 export default function Board () {
+  const onClick = () => {
+    console.log("clicked")
+  }
+
+
   return (
-    <div className="board">Board</div>
+    <div className="board">
+      <Button children={1} onClick={onClick}/>
+    </div>
   )
 }
