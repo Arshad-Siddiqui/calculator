31e3b47145a76c9bacf8c922d269764e1c7a23b7
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-22 18:15:14 +0000

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
