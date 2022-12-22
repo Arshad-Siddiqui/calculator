85e180c39e26e8c022b94cbd5af684c1e1717b05
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-22 21:33:06 +0000

    Give equals button its own color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,9 +3,10 @@ import ButtonProps from "../types/ButtonProps"
 export default function Button (props: ButtonProps) {
   const children = props.children
   const isTall = children == '+' ? 'plus' : ''
+  const isEqual = children == '=' ? 'equal' : ''
 
   return (
-    <button className={`button ${isTall}`} onClick={props.onClick}>
+    <button className={`button ${isTall} ${isEqual}`} onClick={props.onClick}>
       {children}
     </button>
   )
