be97a7eabdfe7a0ee627ee4fc24f423cef55ff48
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-22 19:45:47 +0000

    Represent every Button on Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,40 @@
-import Button from './Button'
+import Button from "./Button";
 
-export default function Board () {
+export default function Board() {
+  const contents = [
+    "C",
+    "AC",
+    "%",
+    "X",
+    7,
+    8,
+    9,
+    "/",
+    4,
+    5,
+    6,
+    "-",
+    1,
+    2,
+    3,
+    "+",
+    0,
+    ".",
+    "=",
+  ];
   const onClick = () => {
-    console.log("clicked")
-  }
-
+    console.log("clicked");
+  };
 
   return (
     <div className="board">
-      <Button children={1} onClick={onClick}/>
+      {contents.map((content, index) => {
+        return (
+          <Button key={index} onClick={onClick}>
+            {content}
+          </Button>
+        );
+      })}
     </div>
-  )
+  );
 }
