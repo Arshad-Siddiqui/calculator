c68b567c2679c29f3f6f793bdf57d12cce7793d8
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-22 18:44:26 +0000

    Show the Board in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Board from "./components/Board"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <Board />
     </div>
   )
 }
