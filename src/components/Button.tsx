91c697a3563331bd197fe48b7e0bede1996fddd9
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-23 20:09:14 +0000

    Make AC button reset calculator

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button (props: ButtonProps) {
   const isEqual = children == '=' ? 'equal' : ''
 
   const handleClick = () => {
-    if (children == 'C') {
+    if (children == 'C' || children == 'AC') {
       setDisplay([0])
       return
     }
