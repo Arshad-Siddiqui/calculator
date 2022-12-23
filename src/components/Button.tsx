fb2a7c105e85fe2ba83d0297afcc5ae87c4492f3
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-23 19:53:22 +0000

    Refactor setDisplay on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,13 @@ import ButtonProps from "../types/ButtonProps"
 
 export default function Button (props: ButtonProps) {
   const children = props.children
+  const setDisplay = props.setDisplay
+
   const isTall = children == '+' ? 'plus' : ''
   const isEqual = children == '=' ? 'equal' : ''
 
   const handleClick = () => {
-    props.setDisplay([children])
+    setDisplay([children])
   }
 
   return (
