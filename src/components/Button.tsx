555e5e13fae227eab5ae68ba19849b5c9fd9d073
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-23 20:07:11 +0000

    Have display store all inputted values

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button (props: ButtonProps) {
   const isEqual = children == '=' ? 'equal' : ''
 
   const handleClick = () => {
-    setDisplay(() => [children])
+    setDisplay((prev) => [...prev, children])
   }
 
   return (
