8f4b1402a001eab06e45272c155a466b5b539293
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-24 13:58:41 +0000

    Setup testing environment for #combineNumbers

---
+++ src/lib/combineNumbers.ts
@@ -0,0 +1,3 @@
+export default function combineNumbers(arr: (number | string)[]): number {
+  return 0
+}
---
+++ src/tests/combineNumbers.test.ts
@@ -0,0 +1,9 @@
+import { it, describe, expect } from 'vitest';
+import combineNumbers from '../lib/combineNumbers';
+
+describe('combineNumbers', () => {
+  it('should combine numbers', () => {
+    const result = combineNumbers([1, 2, 3]);
+    expect(result).toBe(123);
+  });
+})
