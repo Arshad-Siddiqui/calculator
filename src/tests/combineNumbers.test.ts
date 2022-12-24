67bf10d3eabef9643fbcb6eee138b4976b477140
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-24 17:00:55 +0000

    #combineNumbers add further test cases

--- src/tests/combineNumbers.test.ts
+++ src/tests/combineNumbers.test.ts
@@ -15,5 +15,11 @@ describe('combineNumbers', () => {
   it("Should combine numbers and not strings", () => {
     const result = combineNumbers([1, 2, '3']);
     expect(result).toEqual([12, '3']);
+
+    const result2 = combineNumbers([1, '2', 3, 4, '5']);
+    expect(result2).toEqual([1, '2', 34, '5']);
+
+    const result3 = combineNumbers([1, 2, 3, 4, '5', '6', 7, 8, '9']);
+    expect(result3).toEqual([1234, '5', '6', 78, '9']);
   });
 });
