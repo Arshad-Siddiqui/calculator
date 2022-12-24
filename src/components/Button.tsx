0785de8a5cd859c868dbd181a688d4f3b0b7f710
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-24 17:44:11 +0000

    Resets calculator if double operand

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,17 +4,24 @@ import combineNumbers from "../lib/combineNumbers"
 export default function Button (props: ButtonProps) {
   const children = props.children
   const setDisplay = props.setDisplay
+  const display = props.display
 
   const isTall = children == '+' ? 'plus' : ''
   const isEqual = children == '=' ? 'equal' : ''
 
   const handleClick = () => {
+    if (typeof children == 'string' && typeof display[display.length - 1] == 'string') {
+      setDisplay([])
+      return
+    }
+
     if (children == 'C' || children == 'AC') {
       setDisplay([])
       return
     }
 
     setDisplay((prev) => combineNumbers([...prev, children]))
+    console.log(display);
   }
 
   return (
