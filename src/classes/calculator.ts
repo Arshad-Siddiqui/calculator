72f236b2a7761056b891adfd861ec34ebcccd9e7
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-12-26 13:56:53 +0000

    Shorten calculator class

--- src/classes/calculator.ts
+++ src/classes/calculator.ts
@@ -14,24 +14,4 @@ export default class Calculator {
   divide(a: number, b: number) {
     return a / b;
   }
-
-  square(a: number) {
-    return a * a;
-  }
-
-  squareRoot(a: number) {
-    return Math.sqrt(a);
-  }
-
-  cube(a: number) {
-    return a * a * a;
-  }
-
-  cubeRoot(a: number) {
-    return Math.cbrt(a);
-  }
-
-  power(a: number, b: number) {
-    return Math.pow(a, b);
-  }
 }
