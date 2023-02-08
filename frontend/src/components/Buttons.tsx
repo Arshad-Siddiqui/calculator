e2c862a283e6d53a7c8e016ea860b5751e92bbc3
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-02-08 16:40:22 +0000

    Have buttons sit horizontally

--- frontend/src/components/Buttons.tsx
+++ frontend/src/components/Buttons.tsx
@@ -4,8 +4,8 @@ import Operators from "./Operators";
 export default function Buttons ({setDisplay, display}: {setDisplay: Function, display: string[]}) {
   return (
     <div className="buttons">
-      <Operators setDisplay={setDisplay}/>
       <Numpad setDisplay={setDisplay} display={display} />
+      <Operators setDisplay={setDisplay}/>
     </div>
   );
 }
